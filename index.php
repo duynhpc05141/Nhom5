00fd0022b47bf1e419581e1216dd583fae085efc
NgoDuy0810 <148345204+duynhpc05141@users.noreply.github.com>
2023-11-23 15:46:00 +0700

    [F8]_Đăng nhập, tạo tài khoản Google + Captcha

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+ob_start();
+session_start();
 include "./DAO/pdo.php";
+include "./DAO/login.php";
 
 
 if (isset($_GET['act']) && ($_GET['act'] !== "")) {
@@ -7,6 +10,38 @@ if (isset($_GET['act']) && ($_GET['act'] !== "")) {
   $act = $_GET['act'];
   include "includes/header.php";
   switch ($act) {
+    case 'login':
+      if (isset($_POST['login']) && ($_POST['login'])) {
+        $user = $_POST['user_name'];
+        $password = $_POST['user_password'];
+        // Lấy mật khẩu đã băm từ cơ sở dữ liệu dựa trên tên người dùng
+        // Ví dụ, sử dụng một câu lệnh chuẩn bị
+        $dbHashedPassword = fetch_hashed_password_from_database($user);
+        if ($dbHashedPassword && password_verify($password, $dbHashedPassword)) {
+            $check_user = check_user($user);
+            if (is_array($check_user)) {
+                $_SESSION['user_name'] = $check_user;
+                ob_end_clean();
+                header('Location: index.php');
+            } else {
+                $alert = "Đăng nhập thất bại";
+            }
+        } else {
+            $alert = "Tên người dùng hoặc mật khẩu không đúng";
+        }
+    }
+      
+      include './view/account/login.php';
+      break;
+
+    case 'logout':
+      session_unset();
+      header('Location: index.php');
+      break;  
+
+    case 'register':
+      include './view/account/register.php';
+      break;  
     case 'home':
       include "./includes/home.php";
       break;
