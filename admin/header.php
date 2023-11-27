f7ad650d0e9531f98dd3c7a8161f578a524f5dcd
hieuM642004 <136356604+hieuM642004@users.noreply.github.com>
2023-11-27 22:12:16 +0700

    [F16]_Sửa lỗi trang đăng nhập

--- admin/header.php
+++ admin/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,10 @@
     <link href="css/styles.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
 <body>
-  <div class="container">
-  <div class="d-flex" id="wrapper">
+    <div class="container">
+        <div class="d-flex" id="wrapper">
             <!-- Sidebar-->
             <div class="border-end bg-white" id="sidebar-wrapper">
                 <div class="sidebar-heading border-bottom bg-light"><a href="index.php?act=home" class="list-group-item list-group-item-action ">Admin</a></div>
@@ -19,56 +21,53 @@
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=article-list">Bài viết</a>
 
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=user_list">Người dùng</a>
-          <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=comment_list">Bình luận</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=comment_list">Bình luận</a>
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=favourite">Yêu thích</a>
 
 
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="index.php?act=static">Thống kê bài viết</a>
-                    
+
                 </div>
             </div>
             <!-- Page content wrapper-->
             <div id="page-content-wrapper">
                 <!-- Top navigation-->
-                  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                     <div class="container-fluid">
-                        <i id="sidebarToggle" class="fa-solid fa-backward text-secondary" ></i>
+                        <i id="sidebarToggle" class="fa-solid fa-backward text-secondary"></i>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <div class="dropdown" >
-                                        <?php 
-                                        if (isset($_SESSION['user_name'])) {
-                                            extract($_SESSION['user_name']);
-                                            $img = './img/' . $avatar;
-                                            if (file_exists($img)) {
-                                                $hinh = '<img src=" ' . $img . '" alt="Hình ảnh đại diện" height="40px" style="border-radius:10px;">';
-                                              } else {
-                                                $hinh = '0';
-                                              };
-                                        ?>
-                                            <div data-bs-toggle="dropdown">
-                                                <?= $hinh ?>
-                                                <?= $user_name ?>
-                                            </div>
-                                            <ul class="dropdown-menu" style="margin-left: -110px; margin-top: 9px;">
-                                                
-                                               
-                                                <li>
-                                                    <a class="dropdown-item" href="index.php?act=logout">Đăng xuất</a>
-                                                </li>
-                                            </ul>
-                                            
-                                       <?php }?>
-                                    </div>
-                                
-                               
+                                <div class="dropdown">
+                                    <?php
+                                    if (isset($_SESSION['user_name'])) {
+                                        extract($_SESSION['user_name']);
+                                        $img = './img/' . $avatar;
+                                        if (file_exists($img)) {
+                                            $hinh = '<img src=" ' . $img . '" alt="Hình ảnh đại diện" height="40px" style="border-radius:10px;">';
+                                        } else {
+                                            $hinh = '';
+                                        };
+                                    ?>
+                                        <div data-bs-toggle="dropdown">
+                                            <?= $hinh ?>
+                                            <li class="fw-bold" style="cursor: pointer;"><?= $user_name ?></li>
+                                        </div>
+                                        <ul class="dropdown-menu" style="margin-left: -110px; margin-top: 9px;">
+
+
+                                            <li>
+                                                <a class="dropdown-item" href="index.php?act=logout">Đăng xuất</a>
+                                            </li>
+                                        </ul>
+
+                                    <?php } ?>
+                                </div>
+
+
                             </ul>
                         </div>
                     </div>
                 </nav>
                 <!-- Page content-->
-                <div class="container-fluid " >
-
-                   
-              
+                <div class="container-fluid ">
--- admin/login.php
+++ admin/login.php
@@ -5,6 +5,8 @@
     <title> Login Form</title>
     <link rel="stylesheet" href="css/style.css?php echo time(); ?>">
     <script src="https://kit.fontawesome.com/55a9fa42b8.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
@@ -17,50 +19,69 @@
         label.error {
             color: red;
         }
-        .text-capitalize:hover{
+
+        .text-capitalize:hover {
             color: #614BC3;
         }
+
         .alert-error {
-        color: #721c24;
-        background-color: #f8d7da;
-        border-color: #f5c6cb;
-        padding: 10px;
-        margin-bottom: 10px;
-        border: 1px solid transparent;
-        border-radius: 4px;
+            color: #721c24;
+            background-color: #f8d7da;
+            border-color: #f5c6cb;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid transparent;
+            border-radius: 4px;
         }
     </style>
 
-    
+
 
     <div class="container d-flex justify-content-center align-items-center">
         <form class="row g-3 mx-auto shadow p-5 mt-5 mb-3" action="index.php?act=login" method="post" id="login-form">
             <h2 class="text-center">Đăng nhập</h2>
             <div class="col-md-12">
                 <label for="user" class="form-label">Tên đăng nhập</label>
-                <input type="text" spellcheck="true" class="form-control " id="user" placeholder="Tên đăng nhập" required name="user_name">
+                <input type="text" spellcheck="true" class="form-control " id="user" placeholder="Tên đăng nhập"  name="user_name">
 
             </div>
             <div class="col-md-12">
                 <label for="password" class="form-label">Mật khẩu</label>
-                <input type="password" class="form-control" id="password" placeholder="Mật khẩu" required name="user_password">
+                <input type="password" class="form-control" id="password" placeholder="Mật khẩu"  name="user_password">
 
             </div>
 
             <div class="col-12">
 
                 <input type="submit" value="Đăng nhập" name="login" class="btn btn-primary col-12 mb-10">
-               
+
             </div>
-          
-          
+
+
         </form>
     </div>
-     <?php 
-        if (isset($alert) && ($alert != "")) {
-            echo $alert;
+    <?php
+    if (isset($_POST['login']) && ($_POST['login'])) {
+        $user = $_POST['user_name'];
+        $password = $_POST['user_password'];
+        $dbHashedPassword = fetch_hashed_password_from_database($user);
+
+        if ($dbHashedPassword && password_verify($password, $dbHashedPassword)) {
+            $check_admin = check_admin($user);
+
+            if (is_array($check_admin)) {
+                $_SESSION['user_name'] = $check_admin;
+                header('Location: index.php?act=home');
+                exit; // Kết thúc luồng chạy để đảm bảo chuyển hướng hoạt động
+            } else {
+                $alert = 'Tên đăng nhập hoặc mật khẩu sai!!!';
+            }
+        } else {
+            $alert = 'Tên đăng nhập hoặc mật khẩu sai!!!';
         }
-        ?>
+    }
+
+    ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script>
@@ -80,8 +101,22 @@
                 }
             });
         });
+
+
+        var mes = '<?php echo addslashes($alert); ?>';
+
+       
+        Toastify({
+            text: mes,
+            duration: 3000, 
+            close: true,
+            gravity: "top", 
+            position: "center", 
+            backgroundColor: "red",
+        }).showToast();
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+
 </html>
